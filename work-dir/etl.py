be852f25cbd1d802f31714c602a986b28a55a34d
Johnson Hsiung <hsiung.johnson@gmail.com>
2025-08-22 13:19:49 -0700

    finish assessment

--- work-dir/etl.py
+++ work-dir/etl.py
@@ -1,6 +1,8 @@
 ﻿from pyspark.sql.window import Window
-from pyspark.sql.functions import row_number, lit, when, col, split , explode, trim
+from pyspark.sql.functions import row_number, lit, when, col, split , explode, trim, regexp_replace, length
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType, DoubleType, IntegerType, FloatType, LongType, DecimalType
+
 
 
 spark = SparkSession.builder.appName("BoardGamesApp").getOrCreate()
@@ -12,8 +14,24 @@ jdbc_options = {
     "driver": "com.mysql.cj.jdbc.Driver",
 }
 
-bgg_df = spark.read.csv("/opt/spark/files/in/bgg_ranks.csv", header=True, inferSchema=True)
-mmbg_df = spark.read.csv("/opt/spark/files/in/melissa-monfared-board-games.csv", header=True, inferSchema=True, encoding="cp1252") # change encoding if needed
+bgg_df = spark.read.csv(
+    "/opt/spark/files/in/bgg_ranks.csv", 
+    header=True, 
+    inferSchema=True,
+    # quote='"',
+    # escape='"', 
+    # multiLine=False,
+    # encoding="utf-8"
+)
+# bgg_df = bgg_df.withColumn("yearpublished", regexp_replace(col("yearpublished"), '"', ''))
+bgg_df = bgg_df.filter(length(col("yearpublished").cast("string")) <= 4)
+
+mmbg_df = spark.read.csv(
+    "/opt/spark/files/in/melissa-monfared-board-games.csv", 
+    header=True, 
+    inferSchema=True, 
+    encoding="cp1252"
+) # change encoding if needed
 
 
 # drop name column from mmbg
@@ -53,10 +71,10 @@ full_df = joined_df.select(
 "mechanics_name")
 
 #data cleaning
-full_df = full_df.dropDuplicates().dropna(subset=["game_name", "min_players", "max_players", "avg_rating"])
+full_df = full_df.dropDuplicates().dropna(subset=["game_name", "min_players", "year_published", "max_players", "avg_rating"])
 
-print("Full data frame not normalized")
-full_df.show()
+#print("Full data frame not normalized")
+# full_df.show()
 
 
 def make_table_and_bridge(df, column_name):
@@ -78,30 +96,48 @@ def make_table_and_bridge(df, column_name):
 
 domain_df, board_game_domains_df = make_table_and_bridge(full_df, "domain_name")
 mechanics_df, board_game_mechanics_df = make_table_and_bridge(full_df, "mechanics_name")
-domain_df.show()
-mechanics_df.show()
-board_game_domains_df.show()
-board_game_mechanics_df.show()
+# domain_df.show()
+# mechanics_df.show()
+# board_game_domains_df.show()
+# board_game_mechanics_df.show()
 
 
 #BOARD GAMES TABLE
 board_games_df = full_df.drop("domain_name", "mechanics_name")
-print("final board games table")
-board_games_df.show()
-board_games_df.write.format("jdbc").options(**jdbc_options,dbtable="board_game").mode("append").save()
+#print("final board games table")
+#board_games_df.show()
 
+numeric_types = (DoubleType, IntegerType, FloatType, LongType, DecimalType)
+
+for c, t in board_games_df.dtypes:
+    spark_type = board_games_df.schema[c].dataType
+    if isinstance(spark_type, numeric_types) or spark_type == StringType():
+        # Cast string numeric columns to double if necessary
+        board_games_df = board_games_df.withColumn(
+            c,
+            when(col(c) == "NA", None).otherwise(col(c).cast(spark_type))
+        )
+
+
+print("attempting sql inserts. . .")
+
+board_games_df.write.format("jdbc").options(**jdbc_options,dbtable="board_game").mode("append").save()
+print("board game insert successful")
 
 #DOMAIN TABLE
-domain_df.write.format("jdbc").options(**jdbc_options,dbtable="domain").model("append").save()
+domain_df.write.format("jdbc").options(**jdbc_options,dbtable="domain").mode("append").save()
+print("domain insert successful")
 
 #MECHANICS TABLE
-mechanics_df.write.format("jdbc").options(**jdbc_options,dbtable="mechanics").model("append").save()
+mechanics_df.write.format("jdbc").options(**jdbc_options,dbtable="mechanics").mode("append").save()
+print("mechanics insert successful")
 
 #BOARD GAME DOMAINS BRIDGE TABLE
-board_game_domains_df.write.format("jdbc").options(**jdbc_options,dbtable="board_game_domains").model("append").save()
+board_game_domains_df.write.format("jdbc").options(**jdbc_options,dbtable="board_game_domains").mode("append").save()
+print("board game domains insert successful")
 
 #BOARD GAME MECHANICS BRIDGE TABLE
-board_game_mechanics_df.write.format("jdbc").options(**jdbc_options,dbtable="board_game_mechanics").model("append").save()
-
+board_game_mechanics_df.write.format("jdbc").options(**jdbc_options,dbtable="board_games_mechanics").mode("append").save()
+print("board game mechanics insert successful")
 
 spark.stop()
