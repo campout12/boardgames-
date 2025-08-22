e0017bbadf75f18a65282d60df8667e99ad63863
Johnson Hsiung <hsiung.johnson@gmail.com>
2025-08-22 12:14:35 -0700

    etl

--- boardgame.sql
+++ boardgame.sql
@@ -3,7 +3,7 @@ create database board_games;
 use board_games;
 
 create table board_game (
-    board_game_id int primary key auto_increment,
+    board_game_id int primary key,
     game_name varchar(255),
     year_published int,
     min_players int,
@@ -13,27 +13,27 @@ create table board_game (
 );
 
 create table domain (
-    domain_id int primary key auto_increment,
+    domain_name_id int primary key,
     domain_name varchar(255)
 );
 
 create table board_game_domains (
     board_game_id int,
-    domain_id int,
-    primary key (board_game_id, domain_id),
+    domain_name_id int,
+    primary key (board_game_id, domain_name_id),
     foreign key (board_game_id) references board_game(board_game_id),
-    foreign key (domain_id) references domain(domain_id)
+    foreign key (domain_name_id) references domain(domain_name_id)
 );
 
 create table mechanics (
-    mechanics_id int primary key auto_increment,
+    mechanics_name_id int primary key,
     mechanics_name varchar(255)
 );
 
 create table board_games_mechanics (
     board_game_id int,
-    mechanics_id int,
-    primary key (board_game_id, mechanics_id),
+    mechanics_name_id int,
+    primary key (board_game_id, mechanics_name_id),
     foreign key (board_game_id) references board_game(board_game_id),
-    foreign key (mechanics_id) references mechanics(mechanics_id)
+    foreign key (mechanics_name_id) references mechanics(mechanics_name_id)
 );
---
+++ work-dir/etl.py
@@ -0,0 +1,107 @@
+﻿from pyspark.sql.window import Window
+from pyspark.sql.functions import row_number, lit, when, col, split , explode, trim
+from pyspark.sql import SparkSession
+
+
+spark = SparkSession.builder.appName("BoardGamesApp").getOrCreate()
+
+jdbc_options = {
+    "url": spark.conf.get("spark.mysql.board_games.url"),
+    "user": spark.conf.get("spark.mysql.board_games.user"),
+    "password": spark.conf.get("spark.mysql.board_games.password"),
+    "driver": "com.mysql.cj.jdbc.Driver",
+}
+
+bgg_df = spark.read.csv("/opt/spark/files/in/bgg_ranks.csv", header=True, inferSchema=True)
+mmbg_df = spark.read.csv("/opt/spark/files/in/melissa-monfared-board-games.csv", header=True, inferSchema=True, encoding="cp1252") # change encoding if needed
+
+
+# drop name column from mmbg
+mmbg_df =mmbg_df.drop("name")
+
+# merge dataframes
+joined_df = bgg_df.join(mmbg_df, on="id")
+
+joined_df =joined_df.drop("id")
+
+# rename columns
+joined_df= joined_df.withColumnsRenamed({
+    "name": "game_name",
+    "Year Published": "year_published",
+    "Min Players": "min_players",
+    "Max Players": "max_players",
+    "Rating Average": "avg_rating",
+    "rank": "ranking",
+    "Domains": "domain_name",
+    "Mechanics": "mechanics_name"
+})
+
+window = Window.orderBy(lit(1))
+
+joined_df = joined_df.withColumn("board_game_id", row_number().over(window))
+
+#master dataframe with all columns, not normalized
+full_df = joined_df.select(
+"board_game_id",
+"game_name", 
+"year_published", 
+"min_players", 
+"max_players", 
+"avg_rating", 
+"ranking", 
+"domain_name",
+"mechanics_name")
+
+#data cleaning
+full_df = full_df.dropDuplicates().dropna(subset=["game_name", "min_players", "max_players", "avg_rating"])
+
+print("Full data frame not normalized")
+full_df.show()
+
+
+def make_table_and_bridge(df, column_name):
+    table = df.withColumn(
+        column_name, explode(split(col(column_name), ","))
+    ).withColumn( column_name, trim(col(column_name)))
+
+    unique_table = table.select(col(column_name)).distinct()
+    window = Window.orderBy(lit(1)) # generate unique IDs for each unique name
+    unique_table = unique_table.withColumn(f"{column_name}_id", row_number().over(window))
+
+    bridge_table = table.join(
+        unique_table, on=column_name
+    ).select(
+        "board_game_id", f"{column_name}_id"
+    ).dropDuplicates()
+    
+    return unique_table, bridge_table
+
+domain_df, board_game_domains_df = make_table_and_bridge(full_df, "domain_name")
+mechanics_df, board_game_mechanics_df = make_table_and_bridge(full_df, "mechanics_name")
+domain_df.show()
+mechanics_df.show()
+board_game_domains_df.show()
+board_game_mechanics_df.show()
+
+
+#BOARD GAMES TABLE
+board_games_df = full_df.drop("domain_name", "mechanics_name")
+print("final board games table")
+board_games_df.show()
+board_games_df.write.format("jdbc").options(**jdbc_options,dbtable="board_game").mode("append").save()
+
+
+#DOMAIN TABLE
+domain_df.write.format("jdbc").options(**jdbc_options,dbtable="domain").model("append").save()
+
+#MECHANICS TABLE
+mechanics_df.write.format("jdbc").options(**jdbc_options,dbtable="mechanics").model("append").save()
+
+#BOARD GAME DOMAINS BRIDGE TABLE
+board_game_domains_df.write.format("jdbc").options(**jdbc_options,dbtable="board_game_domains").model("append").save()
+
+#BOARD GAME MECHANICS BRIDGE TABLE
+board_game_mechanics_df.write.format("jdbc").options(**jdbc_options,dbtable="board_game_mechanics").model("append").save()
+
+
+spark.stop()
